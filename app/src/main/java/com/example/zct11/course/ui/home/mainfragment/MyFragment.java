2e77191d5492f65f14e3328d3ff9b92d135cb721
LiangZhilin <891004865@qq.com>
2017-10-27 14:47:35 +0800

    完善咨询

--- app/src/main/java/com/example/zct11/course/ui/home/mainfragment/MyFragment.java
+++ app/src/main/java/com/example/zct11/course/ui/home/mainfragment/MyFragment.java
@@ -1,33 +1,68 @@
 package com.example.zct11.course.ui.home.mainfragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.zct11.course.R;
+import com.example.zct11.course.ui.login.LoginActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 /**
  * Created by Administrator on 2017/10/19.
  */
 
-public class MyFragment extends Fragment {
+public class MyFragment extends Fragment implements View.OnClickListener {
 
 
 
+    private Toolbar toolbar;
+    private TextView nologin;
+    private TextView name;
+    private ImageView sexImg;
+    private TextView studyTime;
+    private LinearLayout login;
 
-    public static MyFragment getInstance (){
-        MyFragment myFragment=new MyFragment();
-        return  myFragment;
+    public static MyFragment getInstance() {
+        MyFragment myFragment = new MyFragment();
+        return myFragment;
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_my, null);
+        init(v);
+
         return v;
     }
 
+    private void init(View v) {
+        toolbar= (Toolbar) v.findViewById(R.id.toolbar);
+        nologin= (TextView) v.findViewById(R.id.nologin);
+        name= (TextView) v.findViewById(R.id.name);
+        sexImg= (ImageView) v.findViewById(R.id.sex_img);
+        studyTime= (TextView) v.findViewById(R.id.study_time);
+        login= (LinearLayout) v.findViewById(R.id.login);
+        nologin.setOnClickListener(this);
+    }
+
+
+
+    @Override
+    public void onClick(View view) {
+        startActivity(new Intent(getActivity(), LoginActivity.class));
+    }
 }
---
+++ app/src/main/java/com/example/zct11/course/ui/login/LoginActivity.java
@@ -0,0 +1,144 @@
+package com.example.zct11.course.ui.login;
+
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v8.renderscript.Allocation;
+import android.support.v8.renderscript.Element;
+import android.support.v8.renderscript.RenderScript;
+import android.support.v8.renderscript.ScriptIntrinsicBlur;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.example.zct11.course.R;
+import com.example.zct11.course.widget.statusbar.StatusBarUtil;
+
+
+/**
+ * Created by Administrator on 2017/10/24.
+ */
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+    private EditText inputEmail;
+    private EditText inputPassword;
+    private AppCompatButton btnSign;
+    private ImageView back;
+    private TextView tvRegister;
+    private ScrollView svRoot;
+    private int radius = 25;
+    private ProgressDialog mProgressDialog;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        StatusBarUtil.setTranslucent(this);
+        init();
+
+
+    }
+
+    private void init() {
+        inputEmail= (EditText) findViewById(R.id.input_email);
+        inputPassword= (EditText) findViewById(R.id.input_password);
+        btnSign= (AppCompatButton) findViewById(R.id.btn_sign);
+        tvRegister= (TextView) findViewById(R.id.tv_register);
+        svRoot= (ScrollView) findViewById(R.id.sv_root);
+        back= (ImageView) findViewById(R.id.back);
+        btnSign.setOnClickListener(this);
+        //高斯模糊背景
+        applyBlur();
+    }
+
+
+    @Override
+    public void onClick(View view) {
+       switch (view.getId()){
+           case R.id.back:
+               finish();
+               break;
+           case R.id.btn_sign:
+               if (!validate()) {
+                   return;
+               }
+
+               btnSign.setEnabled(false);
+
+               mProgressDialog = new ProgressDialog(this);
+               mProgressDialog.setIndeterminate(true);
+               mProgressDialog.setMessage("正在验证...");
+               mProgressDialog.show();
+
+               String userword = inputEmail.getText().toString();
+               String password = inputPassword.getText().toString();
+
+               //presenter.login(userword,password);
+               break;
+           case R.id.tv_register:
+
+               break;
+       }
+    }
+
+    /**
+     * 邮箱，密码是否格式正确
+     *
+     * @return
+     */
+    public boolean validate() {
+        boolean valid = true;
+
+        String email = inputEmail.getText().toString();
+        String password = inputPassword.getText().toString();
+
+        if (email.isEmpty() || !Patterns.PHONE.matcher(email).matches()) {
+            inputEmail.setError("请输入有效的手机号码");
+            valid = false;
+        } else {
+            inputEmail.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            inputPassword.setError("密码长度在4-10位之间");
+            valid = false;
+        } else {
+            inputPassword.setError(null);
+        }
+
+        return valid;
+    }
+
+    private void applyBlur() {
+        Drawable db = getResources().getDrawable(R.drawable.bg);
+        BitmapDrawable drawable = (BitmapDrawable) db;
+        Bitmap bgBitmap = drawable.getBitmap();
+        //处理得到模糊效果的图
+        RenderScript renderScript = RenderScript.create(this);
+        // Allocate memory for Renderscript to work with
+        final Allocation input = Allocation.createFromBitmap(renderScript, bgBitmap);
+        final Allocation output = Allocation.createTyped(renderScript, input.getType());
+        // Load up an instance of the specific script that we want to use.
+        ScriptIntrinsicBlur scriptIntrinsicBlur = ScriptIntrinsicBlur.create(renderScript, Element.U8_4(renderScript));
+        scriptIntrinsicBlur.setInput(input);
+        // Set the blur radius
+        scriptIntrinsicBlur.setRadius(radius);
+        // Start the ScriptIntrinisicBlur
+        scriptIntrinsicBlur.forEach(output);
+        // Copy the output to the blurred bitmap
+        output.copyTo(bgBitmap);
+        renderScript.destroy();
+        BitmapDrawable bitmapDrawable = new BitmapDrawable(getResources(), bgBitmap);
+        svRoot.setBackground(bitmapDrawable);
+    }
+}
